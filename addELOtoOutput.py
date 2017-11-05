6a185533475af80b37800f97749ab9357ec65947
rymuelle <rmueller@physics.tamu.edu>
2017-11-04 23:23:38 -0500

    added class, haven't cleaned addElo up yet

--- addELOtoOutput.py
+++ addELOtoOutput.py
@@ -4,6 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import csv
 from scipy.stats import poisson, norm
+#import pickle
 
 reader = csv.DictReader(open("output.csv"), delimiter = '\t') 
 
@@ -11,6 +12,88 @@ f = open('output_step2.csv','wb')
 field_names=["gameId", "homeTeam", "homeId", "awayTeam", "awayId", "year", "week", "homeScore", "awayScore", "nHomeTD", "nAwayTD", "HomeOffenseScore", "HomeDefenseScore", "AwayOffenseScore", "AwayDefenseScore", "homeElo", "awayElo", "predWin", "homeEloFast", "awayEloFast", "predWinFast" ,"homeEloPerformance", "awayEloPerformance" ,"obsWin"]
 w = csv.DictWriter(f,fieldnames=field_names, delimiter='\t')
 w.writeheader()
+
+#crossElo = {}
+
+def get_week(s):
+    try:
+        int(s)
+        return int(s)
+    except ValueError:
+        return 16
+
+class team():
+    
+    def __init__(self, year, week, teamId, teamName):
+        self.elo = [1500]
+        self.eloPerformance = [1500]
+        self.year = [int(year)]
+        self.week = [get_week(week)]
+        self.home = []
+        self.nGames = 0
+        self.id = teamId
+        self.name = teamName
+
+    def probCountAGreaterThanB(self, countA, countB):
+        prob = 0
+        integral_factor = 5
+        for i in range(int(countA+countB)*integral_factor):
+            dprob = poisson.pmf(i, countB)*poisson.sf(i, countA)
+            prob = prob + dprob
+            if dprob < .0001: break
+        return prob
+
+    def obsWinProb(self, countA, countB):
+        homeWinProb = self.probCountAGreaterThanB(countA,countB)
+        awayWinProb = self.probCountAGreaterThanB(countB,countA)
+        homeWinProbAdjusted = (homeWinProb)/(homeWinProb + awayWinProb)
+        return homeWinProbAdjusted
+
+    def computedWinProb(self, homeElo, awayElo):
+        return 1/(10**(-( float(homeElo) - float(awayElo))/400) +1 ) 
+
+    def computeElofromProb(self, prob):
+         return (-math.log(1/prob -1)/math.log(10)*400)
+
+    def addGame(self, newElo, eloPerformance, year, week, home):
+        self.nGames = self.nGames  +1
+        self.elo.append(newElo)
+        self.eloPerformance.append(eloPerformance)
+        self.year.append(int(year))
+        self.week.append(get_week(week))
+        self.home.append(home)
+
+    def returnElo(self):
+        return self.elo[self.nGames]
+
+
+    def newGame(self, score, opponentScore, kFactor, oponenetClass, year, week, verbose):
+        elo = self.elo[self.nGames]
+        score = float(score)/7.0 + .1
+        opponentScore = float(opponentScore)/7.0 + .1
+
+        opponentElo = oponenetClass.returnElo()
+
+        if verbose: print "year: {} week: {}".format(year, week)
+        if verbose: print "home Team: {} away Team: {}".format(self.name, oponenetClass.name)
+       
+        predProb = self.computedWinProb(elo, opponentElo)
+        obsProb = self.obsWinProb(score, opponentScore)
+        if verbose: print "elo: {} opponent elo: {} win prediction: {} score: {} {} obsWinProb: {}".format(elo, opponentElo, predProb, score, opponentScore, obsProb)
+
+        eloPerformance = opponentElo + self.computeElofromProb(obsProb)
+        opponentEloPerformance = elo - self.computeElofromProb(obsProb)
+
+        elo = elo + kFactor*(obsProb - predProb)
+        opponentElo = opponentElo - kFactor*(obsProb-predProb)
+
+        if verbose: print "uptdated elo: {} opponent elo: {} elo performance: {} opponent elo performance: {} k factor: {}".format(elo, opponentElo, eloPerformance, opponentEloPerformance, kFactor)
+        self.addGame(elo, eloPerformance, year, week, True)
+        oponenetClass.addGame(opponentElo, opponentEloPerformance, year, week, False)
+        #add stuff
+
+
+teamDict = {}
 
 double_smoothing = {}
 teamElo = {}
@@ -30,22 +113,54 @@ for row in reader:
         if key in field_names:
             writeDict[key] = row[key]
 
+    year = row['year']
+    week = get_week(row['week'])
+
     homeId = row['homeId']
+    homeTeam = row['homeTeam']
+    awayTeam = row['awayTeam']
     awayId = row['awayId']
     teamID_to_Ab[homeId]=  row['homeTeam']
     teamID_to_Ab[awayId]=  row['awayTeam']
 
+    homeScore = float(row['homeScore'])
+    awayScore = float(row['awayScore'])
+
+    if homeId not in teamDict:
+        teamDict[homeId] = team(year, week, homeId, homeTeam)
+    if awayId not in teamDict:
+        teamDict[awayId] = team(year, week, awayId, awayTeam)
+
+    kFactor = 120 +80/week
+    teamDict[homeId].newGame(homeScore, awayScore, kFactor, teamDict[awayId], year, week, True)
+
     # set default elo to new teams
     if homeId not in teamElo.keys():
+        #crossElo[homeId] = 0
+
+      
         double_smoothing[homeId] = {"st": 0, "st-1":0, "bt": 0, "bt-1":0, "F+": 0}
         teamElo[homeId] = 1500
         teamElo[homeId+"_count"] = 0
         teamEloFast[homeId] = 1500
     if awayId not in teamElo.keys():
+       # crossElo[awayId] = 0
         double_smoothing[awayId] = {"st": 0, "st-1":0, "bt": 0, "bt-1":0, "F+": 0}
         teamElo[awayId] = 1500
         teamElo[awayId+"_count"] = 0
         teamEloFast[awayId] = 1500
+
+    #if homeId > awayId:
+    #    if homeId not in crossElo:
+    #        crossElo[homeId] = {}
+    #    if awayId not in crossElo[homeId]:
+    #        crossElo[homeId][awayId] = 0
+    #if awayId > homeId:
+    #    if awayId not in crossElo:
+    #        crossElo[awayId]  = {}
+    #    if awayId not in crossElo[awayId]:
+    #        crossElo[homeId][homeId] = 0
+
 
     teamElo[awayId+"_count"] = int(teamElo[awayId+"_count"]) + 1 
     teamElo[homeId+"_count"] = int(teamElo[homeId+"_count"]) + 1 
@@ -108,12 +223,7 @@ for row in reader:
         writeDict["obsWin"] = round(adjustedHomeWinProb,3)
     
     
-        def get_week(s):
-            try:
-                int(s)
-                return int(s)
-            except ValueError:
-                return 16
+
     
         #update ELOs
         
@@ -139,8 +249,8 @@ for row in reader:
         chaosFactor = 1
         #chaosFactor = (abs(homeEloFast-homeElo) + abs(awayEloFast - awayElo))/(k)
 
-        homeElo = (homeElo + chaosFactor*k*(homeWinValueObs - homeWinValuePred))
-        awayElo = (awayElo - chaosFactor*k*(homeWinValueObs - homeWinValuePred))
+        homeElo = (homeElo + k*(homeWinValueObs - homeWinValuePred))
+        awayElo = (awayElo - k*(homeWinValueObs - homeWinValuePred))
         #kFast = (k + 160/(week+2))#200/(teamCountTotal + 2) + 80/(week))
         kFast = .04 +.02/week
         if adjustedHomeWinProb > .99: adjustedHomeWinProb = .99
@@ -212,10 +322,11 @@ for row in reader:
 print "\n"
 keylist = teamElo.keys()
 
+
 rank = 1
 eloArray = []
 for key in teamElo:
-    if "count" not in key: eloArray.append([key, teamElo[key]])
+    if "count" not in key: eloArray.append([key, teamEloFast[key]])
 
 
 eloArray.sort(key = lambda x: x[1], reverse=True)
@@ -225,6 +336,7 @@ for team in eloArray:
     print rank, teamID_to_Ab[team[0]], team[1]
     rank = rank +1
 
+print "\n"
 
-
+print crossElo
 f.close
